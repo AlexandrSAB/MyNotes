a76d0c331dcc9b867dbf065b521478d154130112
Alexandr Krylov <b500201@yandex.ru>
2025-12-03 13:40:45 +0300

    Поменял кнопку добавления заметки на FloatingActionButton

---
+++ app/src/main/java/com/example/mynotes/NoteEditActivity.java
@@ -0,0 +1,46 @@
+package com.example.mynotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.CheckBox;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import static com.example.mynotes.Constants.*;
+import android.widget.CheckBox;
+
+public class NoteEditActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.note_editing);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+       /* CheckBox checkBoxImportant;
+        boolean isImportant;
+
+        EditText title = findViewById(R.id.editTtitle);
+        EditText text = findViewById(R.id.editNoteText);
+        checkBoxImportant = findViewById(R.id.editImportant);*/
+
+        Button buttonCancel = findViewById(R.id.buttonCancel);
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
